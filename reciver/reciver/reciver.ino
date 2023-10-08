720d5e527d72bc83325f95306de08955bb61b848
Subhankar sarkar <25subhankar@gmail.com>
2023-10-08 17:23:30 +0530

    add turning move code

--- reciver/reciver/reciver.ino
+++ reciver/reciver/reciver.ino
@@ -1,14 +1,11 @@
 #include <SPI.h>
 #include <RF24.h>
 #include <nRF24L01.h>
-#include <Servo.h>
 
-// Servo esc_signal;
-
-Servo S1;
-Servo S2;
-Servo S3;
-Servo S4;
+int IN1 = 3;
+int IN2 = 2;
+int IN3 = 4;
+int IN4 = 5;
 
 int velocity = 0;
 
@@ -25,15 +22,18 @@ void setup() {
   radio.begin();
   radio.openReadingPipe(0, address);
   radio.startListening();
-
-  S1.attach(9);
-  S2.attach(6);
-  S3.attach(5);
-  S4.attach(3);
-  delay(2000);
-
-
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
   Serial.begin(9600);
+
+
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
 }
 
 struct datapack {
@@ -48,6 +48,50 @@ struct datapack {
 };
 datapack data;
 
+void stopMotors() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  Serial.println("STOP");
+}
+
+void moveForward() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  Serial.println("Go FORWARD");
+}
+
+void moveBackward() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  Serial.println("Go BACKWARD");
+}
+
+
+void turnLeft() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  Serial.println("Turn LEFT");
+}
+
+void turnRight() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  Serial.println("Turn RIGHT");
+}
+
+
+
+
 void loop() {
   while (radio.available()) {
 
@@ -58,36 +102,46 @@ void loop() {
     // digitalWrite(led01, data.toggleSwitch01);
     // digitalWrite(led02, data.toggleSwitch02);
 
-    LeftX = map(data.joystickLeft_x, 0, 1023, 180, 0);
-    LeftY = map(data.joystickLeft_y, 0, 1023, 180, 0);
-    
+    LeftX = map(data.joystickLeft_x, 0, 1023, 0, 180);
+    LeftY = map(data.joystickLeft_y, 0, 1023, 0, 180);
+
+    // Right Joystick
+    RightX = map(data.joystickRight_x, 0, 1023, 0, 180);
+    RightY = map(data.joystickRight_y, 0, 1023, 0, 180);
+
+    // if (LeftY < 95 && LeftY > 89) {
+    if ((LeftY < 95 && LeftY > 89) && (RightY < 95 && RightY > 89)) {
+      stopMotors();
+    }
+    if (LeftY > 95) {
+      moveForward();
+    }
+    if (LeftY < 89) {
+      moveBackward();
+    }
+
+    if (RightX < 89) {
+      turnLeft();
+    }
+    if (RightX > 95) {
+      turnRight();
+    }
+
+
     Serial.print("LX :");
-    Serial.print(data.joystickLeft_x);
     Serial.print(" :");
     Serial.print(LeftX);
     Serial.print("  LY :");
-    Serial.print(data.joystickLeft_y);
     Serial.print(" :");
     Serial.print(LeftY);
 
-    // Right Joystick
-    RightX = map(data.joystickRight_x, 0, 1023, 180, 0);
-    RightY = map(data.joystickRight_y, 0, 1023, 180, 0);
-
     
     Serial.print(" RX :");
-    Serial.print(data.joystickRight_x);
+    Serial.print(RightX);
     Serial.print(" RY :");
-    Serial.print(data.joystickRight_y);
+    Serial.print(RightY);
     Serial.print("\n");
 
     // esc_signal.writeMicroseconds(RightY);
-
-    S1.write(RightX);
-    S2.write(RightY);
-    S3.write(LeftX);
-    S4.write(LeftY);
-
-
   }
 }
